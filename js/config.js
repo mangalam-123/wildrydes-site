dac03d2ac1c5af03a03a3665429ffb4ffad06c28
Priyanka Jaiswal <priyankajazz1997@gmail.com>
2022-11-06 18:57:54 +0530

    changes in config file

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_vdZQT4e6N', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1dea4cmeqri0vm0rlfsndaugi6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_6qbiHM59y', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '7qkkf7t83su986rbvf6k7fkpr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: 'https://ut4qo6nfhg.execute-api.ap-south-1.amazonaws.com/test' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
